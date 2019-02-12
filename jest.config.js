46d304a5eb7a3cd2576006a41e97e74650a26fd6
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-02-12 18:27:39 +0300

    common: configured tests, added pre commit hook, added publish

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,34 @@
 module.exports = {
     setupFiles: [
         '<rootDir>/config/jest/setup/fetch.setup.js'
-    ]
+    ],
+    // This configuration is used to defeat the problem:
+    //  jest-haste-map: @providesModule naming collision:
+    //   Duplicate module name: @eigenspace/jest-screenshot-testing
+    //   Paths: /Users/dsitdikov/Projects/jest-screenshot-testing/package.json collides with /Users/dsitdikov/Projects/jest-screenshot-testing/dist/package.json
+    //  This warning is caused by a @providesModule declaration with the same name across two different files.
+    modulePathIgnorePatterns: [
+        '<rootDir>/dist/'
+    ],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'src/tsconfig.spec.json'
+        }
+    },
+    testMatch: [
+        '<rootDir>/src/**/?(*.)(spec).(ts|tsx)'
+    ],
+    moduleFileExtensions: [
+        'web.ts',
+        'ts',
+        'tsx',
+        'web.js',
+        'js',
+        'json',
+        'node'
+    ],
+    transform: {
+        '^(?!.*\\.(js|ts|tsx|css|json)$)': '<rootDir>/config/jest/transform/file.transform.js',
+        '^.+\\.tsx?$': '<rootDir>/config/jest/transform/typescript.transform.js'
+    }
 };
