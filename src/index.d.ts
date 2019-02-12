b8b6f74dec24ac53e82cce2f51582a04d91a0001
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-02-12 19:37:40 +0300

    common: updated screenshot-matcher reference

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,6 @@ declare module 'jest-screenshot-testing';
 
 // We manually set out ts-bundle because dts-bundle plugin doesn't work.
 // @ts-ignore
-export * from './types/screenshot-matcher/react/react-to-match-image-snapshot.setup';
+export * from './types/screenshot-matcher/react/react-to-match-image-snapshot';
 // @ts-ignore
 export * from './types/screenshot-matcher/react/react-matcher-config';
